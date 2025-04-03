81bd54f3c247b4651139f08d20ddaa9d8379cd5d
Adam Bridgers <16521414+awbridgers@users.noreply.github.com>
2025-04-03 15:11:40 -0400

    change order

--- src/repos.ts
+++ src/repos.ts
@@ -1,105 +1,106 @@
-import liveStatsTracker from './images/live-stats-tracker.png'
-import lineupDataViewer from './images/lineup-data-viewer.png'
-import movieRatigns from './images/movie-ratings.png'
-import bsdBracketChallenge from './images/bsd-bracket-challenge.png'
-import tacticsTrainer from './images/tactics-trainer.png'
-import sc2Stats from './images/sc2-stats.png'
-import vocabList from './images/vocab-list.png'
-import tacticsApp from './images/tactics-app.png'
-import football from './images/football-analysis.png'
-import statTracker from './images/stat-tracker.png'
-import atomChess from './images/atom-chess.png'
+import liveStatsTracker from './images/live-stats-tracker.png';
+import lineupDataViewer from './images/lineup-data-viewer.png';
+import movieRatigns from './images/movie-ratings.png';
+import bsdBracketChallenge from './images/bsd-bracket-challenge.png';
+import tacticsTrainer from './images/tactics-trainer.png';
+import sc2Stats from './images/sc2-stats.png';
+import vocabList from './images/vocab-list.png';
+import tacticsApp from './images/tactics-app.png';
+import football from './images/football-analysis.png';
+import statTracker from './images/stat-tracker.png';
+import atomChess from './images/atom-chess.png';
 
 export const repos = [
   {
-    title: "StatTracker",
-    repoURL: 'https://github.com/awbridgers/StatTracker',
-    pagesURL: 'https://github.com/awbridgers/StatTracker/releases',
-    image: statTracker,
-    description: 'An electron version of the Live Stats Tracker. This version allows users to save the roster and starting lineups locally.'
-
-  },
-  {
-    title: "Atom Chess",
-    repoURL: 'https://github.com/awbridgers/atom-chess',
-    pagesURL: 'https://github.com/awbridgers/atom-chess/releases',
-    image: atomChess,
-    description: 'An electron version Chess GUI with Stockfish 17 built in.'
-
-  },
-  {
-    title: "Live Stats Tracker",
-    repoURL: 'https://github.com/awbridgers/live-stats-tracker',
-    pagesURL: 'https://awbridgers.github.io/live-stats-tracker',
-    image: liveStatsTracker,
-    description: 'An app to convert basketball play by play data from LiveStats into useable raw data in a spreadsheet format.'
-
-  },
-  
-  {
-    title: "Lineup Data Viewer",
+    title: 'Lineup Data Viewer',
     repoURL: 'https://github.com/awbridgers/lineup-data-viewer',
     pagesURL: 'https://awbridgers.github.io/lineup-data-viewer',
     image: lineupDataViewer,
-    description: 'An app that takes raw data basketball data from Firebase and computes it into advanced statistics for easy viewing and analysis.'
-
+    description:
+      'An app that takes raw data basketball data from Firebase and computes it into advanced statistics for easy viewing and analysis.',
   },
-  
   {
-    title: "Movie Ratings",
+    title: 'Atom Chess',
+    repoURL: 'https://github.com/awbridgers/atom-chess',
+    pagesURL: 'https://github.com/awbridgers/atom-chess/releases',
+    image: atomChess,
+    description: 'An electron version Chess GUI with Stockfish 17 built in.',
+  },
+  {
+    title: 'StatTracker',
+    repoURL: 'https://github.com/awbridgers/StatTracker',
+    pagesURL: 'https://github.com/awbridgers/StatTracker/releases',
+    image: statTracker,
+    description:
+      'An electron version of the Live Stats Tracker. This version allows users to save the roster and starting lineups locally.',
+  },
+
+  {
+    title: 'Live Stats Tracker',
+    repoURL: 'https://github.com/awbridgers/live-stats-tracker',
+    pagesURL: 'https://awbridgers.github.io/live-stats-tracker',
+    image: liveStatsTracker,
+    description:
+      'An app to convert basketball play by play data from LiveStats into useable raw data in a spreadsheet format.',
+  },
+
+  {
+    title: 'Movie Ratings',
     repoURL: 'https://github.com/awbridgers/movie-ratings',
     pagesURL: 'https://awbridgers.github.io/movie-ratings',
     image: movieRatigns,
-    description: 'A Rotten Tomatoes style app where users can log on and rate movies watched at "Cage Club." Most movies include American icon, Nicolas Cage.'
-
+    description:
+      'A Rotten Tomatoes style app where users can log on and rate movies watched at "Cage Club." Most movies include American icon, Nicolas Cage.',
   },
   {
-    title: "React Native Tactics",
+    title: 'React Native Tactics',
     repoURL: 'https://github.com/awbridgers/tactics-app',
-    pagesURL: 'https://www.dropbox.com/scl/fi/3t8ejiqnpa1gjek65umtv/tactics-trainer.apk?rlkey=79aonubufscl9mbve846ze06k&st=3od8pwhd&dl=0',
+    pagesURL:
+      'https://www.dropbox.com/scl/fi/3t8ejiqnpa1gjek65umtv/tactics-trainer.apk?rlkey=79aonubufscl9mbve846ze06k&st=3od8pwhd&dl=0',
     image: tacticsApp,
-    description: 'A React Native tactics trainer to improve your chess on android.'
-
+    description:
+      'A React Native tactics trainer to improve your chess on android.',
   },
   {
-    title: "Vocab App",
+    title: 'Vocab App',
     repoURL: 'https://github.com/awbridgers/vocab-list',
-    pagesURL: 'https://www.dropbox.com/scl/fi/x8bdr1rb881tev5ablbfr/VocabList.apk?rlkey=wfp5gv98hw0kqxeyxzeg8wp7t&dl=0',
+    pagesURL:
+      'https://www.dropbox.com/scl/fi/x8bdr1rb881tev5ablbfr/VocabList.apk?rlkey=wfp5gv98hw0kqxeyxzeg8wp7t&dl=0',
     image: vocabList,
-    description: 'A React Native app to store words and improve the user\'s vocabulary.'
-
+    description:
+      "A React Native app to store words and improve the user's vocabulary.",
   },
   {
-    title: "Starcraft II Stat Search",
+    title: 'Starcraft II Stat Search',
     repoURL: 'https://github.com/awbridgers/sc2_name_lookup',
-    pagesURL: 'https://www.dropbox.com/scl/fi/p1lymv8qdy6v8ctj90zi7/SC2-Stats.apk?rlkey=un8ar0x12vqi8mqcgpvs21jkf&dl=0',
+    pagesURL:
+      'https://www.dropbox.com/scl/fi/p1lymv8qdy6v8ctj90zi7/SC2-Stats.apk?rlkey=un8ar0x12vqi8mqcgpvs21jkf&dl=0',
     image: sc2Stats,
-    description: 'A React Native app that uses the Blizzard API to search for Starcraft II records and rankings and display them in a table.'
-
+    description:
+      'A React Native app that uses the Blizzard API to search for Starcraft II records and rankings and display them in a table.',
   },
   {
-    title: "Tactics Trainer",
+    title: 'Tactics Trainer',
     repoURL: 'https://github.com/awbridgers/tactics-trainer-online',
     pagesURL: 'https://awbridgers.github.io/tactics-trainer-online',
     image: tacticsTrainer,
-    description: 'A chess game that helps users improve their ability to spot and execute tactics.'
-
+    description:
+      'A chess game that helps users improve their ability to spot and execute tactics.',
   },
   {
-    title: "BSD Bracket Challenge",
+    title: 'BSD Bracket Challenge',
     repoURL: 'https://github.com/awbridgers/bsd-bracket-challenge',
     pagesURL: 'https://awbridgers.github.io/bsd-bracket-challenge',
     image: bsdBracketChallenge,
-    description: 'A bracket challenge made for BSD readers during the cancelled March Madness Tournament of 2020. Users selected and submitted a bracket which was then simulated using March Madness 2008 on XBox 360.'
-
+    description:
+      'A bracket challenge made for BSD readers during the cancelled March Madness Tournament of 2020. Users selected and submitted a bracket which was then simulated using March Madness 2008 on XBox 360.',
   },
   {
-    title: "Football Analysis",
+    title: 'Football Analysis',
     repoURL: 'https://github.com/awbridgers/football-analysis',
     pagesURL: 'https://awbridgers.github.io/football-analysis',
     image: football,
-    description: 'An app used to track football plays based on down, distance, play type, direction, and result. The plays are returned in spreadsheet format for easy sorting'
-
+    description:
+      'An app used to track football plays based on down, distance, play type, direction, and result. The plays are returned in spreadsheet format for easy sorting',
   },
-  
-]
+];
